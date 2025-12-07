f42fdd6216261a621268a2c922496a72253d02a1
aditi desai <me.aditi.desai@gmail.com>
2025-12-07 14:25:02 -0500

    03 finished

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to the app!</Text>
+    </View>
+  );
+}
